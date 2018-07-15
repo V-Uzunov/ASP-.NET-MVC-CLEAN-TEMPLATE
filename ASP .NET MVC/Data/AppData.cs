79084cf5d81e26a48bfd40b31a683ab9f5804c4d
V-Uzunov <vega_99@abv.bg>
2018-07-15 13:48:33 +0300

    Add App Data for Entities

---
+++ ASP .NET MVC/Data/AppData.cs
@@ -0,0 +1,56 @@
+﻿namespace Data
+{
+    using Data.Models;
+    using Data.Repositories;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+
+    public class AppData : IAppData
+    {
+        private readonly DbContext context;
+        private Dictionary<Type, object> repositories;
+
+        public AppData(DbContext context)
+        {
+            this.context = context;
+            this.repositories = new Dictionary<Type, object>();
+        }
+
+        public IRepository<User> Users
+        {
+            get
+            {
+                return this.GetRepository<User>();
+            }
+        }
+        
+        public IRepository<IdentityRole> Roles
+        {
+            get
+            {
+                return this.GetRepository<IdentityRole>();
+            }
+        }
+
+        
+
+        private IRepository<T> GetRepository<T>() where T : class
+        {
+            var typeOfRepository = typeof(T);
+            if (!this.repositories.ContainsKey(typeOfRepository))
+            {
+                var newRepository = Activator.CreateInstance(typeof(Repository<T>), context);
+                this.repositories.Add(typeOfRepository, newRepository);
+            }
+
+            return (IRepository<T>) this.repositories[typeOfRepository];
+        }
+
+        public int SaveChanges()
+        {
+            return this.context.SaveChanges();
+        }
+    }
+}
---
+++ ASP .NET MVC/Data/IAppData.cs
@@ -0,0 +1,19 @@
+﻿namespace Data
+{
+    using Data.Models;
+    using Data.Repositories;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public interface IAppData
+    {
+        IRepository<User> Users
+        {
+            get;
+        }
+
+        IRepository<IdentityRole> Roles
+        {
+            get;
+        }
+    }
+}
