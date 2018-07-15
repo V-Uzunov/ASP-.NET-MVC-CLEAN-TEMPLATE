076c27bb86b2ee6de538085e006b22615980146a
V-Uzunov <vega_99@abv.bg>
2018-07-15 13:24:41 +0300

    Add view engine config and add localization route

--- ASP .NET MVC/Web/App_Start/RouteConfig.cs
+++ ASP .NET MVC/Web/App_Start/RouteConfig.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-
-namespace Web
+﻿namespace Web
 {
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.LowercaseUrls = true;
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //Uncomment for localization
+            //routes.MapRoute(
+            //    name: "CultureDefault",
+            //    url: "{lang}/{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+            //    constraints: new { lang = "bg|en" }
+            //);
 
             routes.MapRoute(
                 name: "Default",
