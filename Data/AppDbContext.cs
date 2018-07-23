b1dee8ce715aceebb6341e01c2ef39b25a88f3b9
V-Uzunov <vega_99@abv.bg>
2018-07-23 12:20:07 +0300

    Add Entities Data Modified and Deleted

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,6 +2,8 @@ namespace Data
 {
     using Data.Models;
     using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Linq;
 
     public class AppDbContext : IdentityDbContext<User>
     {
@@ -13,6 +15,27 @@ namespace Data
         public static AppDbContext Create()
         {
             return new AppDbContext();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.UtcNow;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.UtcNow;
+                }
+            }
         }
     }
 }
